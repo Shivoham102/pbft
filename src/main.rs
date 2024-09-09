742626722647140220dd1fefb18226edf185f277
Shivoham Angal <shivohamangal@gmail.com>
2024-09-09 16:23:17 -0700

    first commit

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use std::io;
+
+fn main() {
+    println!("Hello, world!");
+
+    let mut input = String::new();
+
+    io::stdin().read_line(&mut input).expect("Failed to read input");
+    print!("{input}");
+}
